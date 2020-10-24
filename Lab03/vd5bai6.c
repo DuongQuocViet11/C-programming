2bd351f0182622d0254c5f3c95b24077002ed9e3
Quoc Viet <duongquocv@gmail.com>
2020-10-24 11:41:47 +0700

    Lab03

---
+++ Lab03/vd5bai6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main()
+{
+	int a = 80;
+	char b= 'C';
+	printf ("\nThis is the number stored in 'a' %d", a);
+	printf ("\nTHis is a character interpreted from 'a' %c", a);
+	printf ("\nTHis is also a character storednin 'b' %c", b);
+	printf ("\nHey! The character of'b' is printed as a number! %d", b);
+	return 0;
+}
---
+++ Lab03/vd8bai6.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void main()
+{
+	int i;
+	float x;
+	char c;
+	scanf ("%3d %5f %c", &i, &x, &c);
+	printf ("The value accepted are: %3d, %5f, %c", i, x, c);
+	return 0;
+}
