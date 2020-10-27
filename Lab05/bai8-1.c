abe5b15dbf26860f30281e4a2c0870a8a76c16ad
Quoc Viet <duongquocv@gmail.com>
2020-10-27 10:00:48 +0700

    Update bai8-1.c

--- Lab05/bai8-1.c
+++ Lab05/bai8-1.c
@@ -6,12 +6,13 @@ int main ()
 	scanf ("%d", &x);
 	printf ("Nhap vao so y: ");
 	scanf ("%d", &y);
-	if ((x<2000 && x>3000) && (y>100 && y<500))
-	{
+	if (x<=2000 || x>=3000)
 	
 	   printf ("So x la: %d", x);
-	   printf ("So y la: %d", y);
-    }
+	
+	if (y >= 100 || y <= 500)
+	   printf ("\nso y la: %d", y);
+    
 	else 
 	   printf ("Error");   
     return 0;
